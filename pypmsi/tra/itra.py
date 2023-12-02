a8899d680818059747640660ff887b2f9f1a80b3
GuillaumePressiat <guillaume.pressiat@gmail.com>
2023-12-02 15:24:10 +0100

    fix #17
    #17

--- pypmsi/tra/itra.py
+++ pypmsi/tra/itra.py
@@ -2,6 +2,7 @@
 import polars as pl
 from pypmsi.utils import *
 import re
+import polars.selectors as cs
 
 def itra(finess, annee : int, mois : int, path : str, champ : str = "mco", filepath = "", n_rows = None):
 
@@ -29,7 +30,20 @@ def itra(finess, annee : int, mois : int, path : str, champ : str = "mco", filep
         file_in = (
             path + "/" + str(finess) + "." + str(annee) + "." + str(mois) + "." + ext
         )
-    
+
+    if ('{annee}{mois:02}'.format(annee = annee, mois = mois) >= '202303') & (champ == 'mco'):
+        df = (
+            pl.read_csv(file_in, has_header=False, skip_rows=0, 
+                        separator = ";", dtypes= [pl.Utf8 for i in range(7)],
+                        new_columns=['cle_rsa', 'norss', 'nas', 
+                                     'dtent', 'dtsort', 'ghm1', 'filler'], 
+                        n_rows = n_rows)
+            .with_columns(pl.all().str.strip_chars())
+            .with_columns(cs.starts_with('dt').str.strptime(pl.Date, format="%d%m%Y", strict=False))
+            .drop('filler')
+        )
+        return df
+
     df = pl.read_csv(file_in, has_header=False, skip_rows=0, new_columns=["l"], n_rows = n_rows)
     
     df = parse_pmsi_fwf(df, champ, table, 'xxxx')
